44fa649b41d5f11532148ba2f66c364f2791e80b
Adolfo JG Hernandez <adolfo.jg.hernandez@gmail.com>
2022-05-23 21:34:54 -0400

    Inicializando Repo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import Template from './templates/Template.js';
-console.log('hola');
+import Template from '@templates/Template.js'; //Se utiliza alias que se encuentra en el webpack.config
+import github from '@images/github.png';
+import '@styles/main.css';
+import '@styles/vars.styl';
 
 (async function App() {
   const main = null || document.getElementById('main');
